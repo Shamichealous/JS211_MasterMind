be2c0d48bd5df29b34b435fcbaeae07cd555f748
shamichealous <ishamichealous@gmail.com>
2022-10-17 21:10:24 -0500

    finished up mastermind

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const rl = readline.createInterface({
 let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let correctLetterLocations = 0
+let correctLetters = 0
 
 const printBoard = () =>  {
   for (let i = 0; i < board.length; i++) {
@@ -28,14 +30,57 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
+const generateHint = (guess) =>  {
   // your code here
+  // created two varibales that split up passed in arguments
+  let solutionArray = solution.split('')
+  let guessArray = guess.split('')
+
+  
+  for (let i = 0; i < solutionArray.length; i++){
+    if(solutionArray[i] === guessArray[i]){
+      correctLetterLocations++ 
+      solutionArray[i] = null
+    }
+  }
+  for(let i = 0; i < solutionArray.length; i++){
+    let guessedLetter = guessArray[i]
+    let targetIndex = solutionArray.indexOf(guessedLetter)
+    if(targetIndex > -1){
+      correctLetters++
+      solutionArray[targetIndex] = null
+    }
+  }
+  const hint = `your guess was ${guess} number of correct locations ${correctLetterLocations} 
+  numbers of letters NOT in the correct location ${correctLetters}`
+
+
+  board.push(hint)
+  correctLetters = 0
+  correctLetterLocations = 0
 }
 
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
+  // solution = 'abcd'; // Comment this out to generate a random solution
+  if(solution == guess){
+    console.log('you guessed it!')
+    return 'You guessed it!';
+  } else if (board.length == 10){
+    console.log('you ran out of turns!')
+    return 'You ran out of turns!'
+    
+    } else {
+      generateHint(guess)
+      return 'Guess again.'
+      
+    }
   // your code here
 }
+
+// rules
+// right character and location
+// wrong character but right location
+// flat out wrong
 
 
 const getPrompt = () =>  {
